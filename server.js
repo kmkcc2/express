b7152d6be16a0be31250e299a541f5d7f5c4d245
kmkcc2 <kmk.krowicki@gmail.com>
2023-08-10 08:37:17 +0200

    added cors

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const usersRoutes = require("./routes/users.js");
-// const cors = require("cors");
+const cors = require("cors");
 
 const app = express();
 const PORT = 3000;
 
 app.use(bodyParser.json());
-// app.use(cors());
+app.use(cors());
 
 const userRouter = usersRoutes;
 app.use("/users", userRouter);
