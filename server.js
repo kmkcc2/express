b2827766c77cb02ae4182ce2b8778f9ae8525897
kmkcc2 <kmk.krowicki@gmail.com>
2023-08-11 09:55:54 +0200

    KACPER-1 port and db details stored in .env

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const usersRoutes = require("./routes/users.js");
 const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT;
 
 app.use(bodyParser.json());
 app.use(cors());
