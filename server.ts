8823ccdad1140c8ec895520dc55c5fa2be0c2980
kmkcc2 <kmk.krowicki@gmail.com>
2023-08-31 12:07:22 +0200

    added middleware to verify content-type header

--- server.ts
+++ server.ts
@@ -5,6 +5,7 @@ import authRoutes from './routes/auth'
 import cors from 'cors'
 import dotenv from 'dotenv'
 import authorize from './middleware/authorize'
+import verifyContent from './middleware/verify-content-type'
 dotenv.config()
 
 const app = express()
@@ -14,8 +15,8 @@ app.use(bodyParser.json())
 app.use(cors())
 
 const userRouter = usersRoutes
-app.use('/users', authorize, userRouter)
-app.use('/', authRoutes)
+app.use('/users', verifyContent, authorize, userRouter)
+app.use('/', verifyContent, authRoutes)
 
 app.listen(PORT, () => {
   console.log(`server running on port: http://localhost:${PORT}`)
