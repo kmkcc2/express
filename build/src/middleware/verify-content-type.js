95315eb8fd9b465541a84dfa8f6ea201d5ee93ff
kmkcc2 <kmk.krowicki@gmail.com>
2023-09-15 09:05:11 +0200

    removed build and coverage from tracking; code refactor

--- build/src/middleware/verify-content-type.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = (req, res, next) => {
-    if ((req.method !== 'GET' && req.method !== 'DELETE') && !req.is('application/json')) {
-        return res.status(415).send({ message: 'Invalid Content-Type header' });
-    }
-    next();
-};
